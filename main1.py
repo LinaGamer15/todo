b92dc6ab46693d3dc1159538f14dcea023046c29
LinaGamer15 <71581584+LinaGamer15@users.noreply.github.com>
2021-07-29 18:07:16 +0400

    Delete main1.py

--- main1.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime, timedelta
-from dateutil.relativedelta import relativedelta
-
-print(datetime.now().strftime('%Y %m %d, %H %M %S'))
-date = datetime.now() + timedelta(days=1)
-print(date.strftime('%Y %m %d, %H %M %S'))
-date1 = datetime.now() + relativedelta(months=1)
-print(date1.strftime('%Y %m %d, %H %M %S'))
-print(datetime.now() < date1)
